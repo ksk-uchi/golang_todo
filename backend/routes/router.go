223bd89ed32e3e20de4e76caa0ee16c514314cb4
ksk-uchi <ksk.uchi+github@gmail.com>
2026-02-08 12:05:28 +0900

    feat: CORS 対策

--- backend/routes/router.go
+++ backend/routes/router.go
@@ -1,7 +1,10 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 )
 
 func NewRouter(todoR *TodoRouter) *Router {
@@ -15,5 +18,24 @@ type Router struct {
 }
 
 func (r *Router) Setup(e *echo.Echo) {
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPatch,
+			http.MethodPut,
+			http.MethodDelete,
+			http.MethodOptions,
+		},
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAuthorization,
+			"X-CSRF-Token",
+		},
+		AllowCredentials: true,
+	}))
 	r.todo.SetupTodoRoute(e.Group("/todo"))
 }
