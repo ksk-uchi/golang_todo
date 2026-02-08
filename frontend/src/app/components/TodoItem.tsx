c295effd62aa0e0d96570b32fb40302a8c44d030
ksk-uchi <ksk.uchi+github@gmail.com>
2026-02-08 16:50:15 +0900

    feat: ToDo カードの UI を微修正

--- frontend/src/app/components/TodoItem.tsx
+++ frontend/src/app/components/TodoItem.tsx
@@ -1,7 +1,9 @@
-import { Card } from "@/app/components/ui/card";
+import { Card, CardContent, CardAction } from "@/app/components/ui/card";
 import { Button } from "@/app/components/ui/button";
 import { Trash2 } from "lucide-react";
 import { Todo } from "@/types";
+import { cn } from "@/lib/utils";
+import { TypographyP } from "@/app/components/ui/typography";
 
 interface TodoItemProps {
   todo: Todo;
@@ -11,26 +13,30 @@ interface TodoItemProps {
 
 export function TodoItem({ todo, onDelete, onClick }: TodoItemProps) {
   return (
-    <Card className="px-4 py-2 flex items-center justify-between hover:bg-muted/50 transition-colors group min-h-[3rem]">
-      <div
-        className="flex-1 cursor-pointer font-medium truncate mr-4"
+    <Card className="px-4 py-2 flex justify-between hover:bg-muted/50 transition-colors group min-h-[3rem]">
+      <CardContent
+        className={cn(
+          "flex-1 cursor-pointer font-medium truncate mr-4 columns-2",
+        )}
         onClick={() => onClick(todo)}
       >
-        {todo.title}
-      </div>
-      <div className="opacity-0 group-hover:opacity-100 transition-opacity flex items-center">
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={(e) => {
-            e.stopPropagation();
-            onDelete(todo.id);
-          }}
-          className="text-muted-foreground hover:text-destructive h-8 w-8"
-        >
-          <Trash2 className="h-4 w-4" />
-        </Button>
-      </div>
+        <TypographyP text={todo.title} />
+        <CardAction className={cn("flex items-center")}>
+          <Button
+            variant="ghost"
+            size="icon"
+            className={cn(
+              "text-muted-foreground hover:text-destructive cursor-pointer h-8 w-8",
+            )}
+            onClick={(e) => {
+              e.stopPropagation();
+              onDelete(todo.id);
+            }}
+          >
+            <Trash2 className="h-4 w-4" />
+          </Button>
+        </CardAction>
+      </CardContent>
     </Card>
   );
 }
