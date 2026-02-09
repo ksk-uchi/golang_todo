2b33a60d701f7b6e58e6eea3ee8a6bd66d23d6c6
ksk-uchi <ksk.uchi+github@gmail.com>
2026-02-09 13:50:16 +0900

    Create users table schema

---
+++ backend/ent/schema/user.go
@@ -0,0 +1,31 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("name").NotEmpty(),
+		field.String("email").Unique().NotEmpty(),
+		field.String("password").NotEmpty(),
+		field.Time("created_at").Default(time.Now),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("todos", Todo.Type),
+	}
+}
