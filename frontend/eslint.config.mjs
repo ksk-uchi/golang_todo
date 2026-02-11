757967bc121580c1862534eac85db35debe3ab12
ksk-uchi <ksk.uchi+github@gmail.com>
2026-02-11 15:24:19 +0900

    frontend の Lint や Prettier を少し見直し

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import eslint from "@eslint/js";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import reactPlugin from "eslint-plugin-react";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tslint from "typescript-eslint";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +16,16 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  reactPlugin.configs.flat.recommended,
+  {
+    files: ["src/**/*.ts", "src/**/*.tsx"],
+    extends: [eslint.configs.recommended, tslint.configs.recommended],
+    rules: {
+      "@typescript-eslint/array-type": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/no-danger": "error",
+    },
+  },
 ]);
 
 export default eslintConfig;
